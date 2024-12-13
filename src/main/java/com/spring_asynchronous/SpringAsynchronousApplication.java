c743568381f15aa1e5b01f1d3890c3af1fac922a
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-12-12 22:01:27 -0600

    starting new project spring_asynchronous

---
+++ src/main/java/com/spring_asynchronous/SpringAsynchronousApplication.java
@@ -0,0 +1,13 @@
+package com.spring_asynchronous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAsynchronousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAsynchronousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_asynchronous/SpringAsynchronousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_asynchronous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAsynchronousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
