815d37f725a1a82373596651e1c9824e0c2b0eaa
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-12-12 22:40:56 -0600

    Spring uses the jackson JSON library to convert GitHub's JSON response into a User object

--- src/main/java/com/spring_asynchronous/User.java
+++ src/main/java/com/spring_asynchronous/User.java
@@ -1,4 +1,9 @@
 package com.spring_asynchronous;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
+  private String name;
+  private String blog;
 }
