965af1cdb8091393703a6e2862974687c6aeba9c
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-12-12 23:06:35 -0600

    the GitHubLookupService class uses Spring’s RestTemplate to invoke a remote REST point (api.github.com/users/) and then convert the answer into a User object. Spring Boot automatically provides a RestTemplateBuilder that customizes the defaults with any auto-configuration bits

--- src/main/java/com/spring_asynchronous/GitHubLookupService.java
+++ src/main/java/com/spring_asynchronous/GitHubLookupService.java
@@ -1,4 +1,28 @@
 package com.spring_asynchronous;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@Service
 public class GitHubLookupService {
+
+  private final RestTemplate restTemplate;
+
+  public GitHubLookupService(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
+
+  @Async
+  public CompletableFuture<User> findUser(String user) throws InterruptedException {
+    log.info("looking up > {}", user);
+    String url = String.format("https://api.github.com/users/%s", user);
+    User response = restTemplate.getForObject(url, User.class);
+    Thread.sleep(1000L);
+    return CompletableFuture.completedFuture(response);
+  }
 }
